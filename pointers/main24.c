db2b87263020ea6ff630491a0e4e1d19588aff64
felix <felixthomas173@gmail.com>
2025-08-24 22:43:09 +0530

    24 AUG 2025

---
+++ pointers/main24.c
@@ -0,0 +1,41 @@
+/*Implement your own version of strstr using pointers.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void getString(char **str) {
+	char temp[30];
+	scanf(" %[^\n]",temp);
+	temp[strlen(temp)+1] = '\0';
+	int n = strlen(temp);
+	*str = (char *)calloc(n,sizeof(char));
+	if(!*str) {
+		printf("memory allocation failed!\n");
+		exit(0);
+	}
+	strcpy(*str,temp);
+}
+
+char *subString(char *str,int l,int r) {
+	char *temp;
+	int i,j;
+	temp = (char *)calloc(r-l,sizeof(char));
+	for(i=l;i<r+1;i++) {
+		temp[j++]=*(str+i);
+	}
+	temp[j]='\0';
+	return temp;
+}
+
+int main() {
+	char *str=NULL,*newstr=NULL;
+	int l,r;
+	printf("enter the string : ");
+	getString(&str);
+	printf("starting index and ending index : ");
+	scanf("%d%d",&l,&r);
+	newstr = subString(str,l,r);
+	printf("new substring = %s",newstr);
+	return 0;
+}
